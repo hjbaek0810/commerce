2cbd614dea30eb9b959fb9d6b487a79e993f6745
hjbaek0810 <doolgi0810@naver.com>
2024-11-12 00:50:04 +0900

    Feat: Product list api 작성

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -4,8 +4,9 @@ import { deleteImages } from '@actions/upload';
 import connectDB from '@api/config';
 import ProductModel from '@api/models/product';
 
+import type { NextRequest } from 'next/server';
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   const data = await req.json();
 
   try {
@@ -27,18 +28,36 @@ export async function POST(req: Request) {
   }
 }
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   try {
     await connectDB();
-    const products = await ProductModel.find().sort('-createdAt'); // -는 desc
+
+    const { searchParams } = req.nextUrl;
+
+    const pageNumber = Number(searchParams.get('page'));
+    const limitNumber = Number(searchParams.get('limit'));
+
+    const products = await ProductModel.find()
+      .limit(limitNumber)
+      .skip((pageNumber - 1) * limitNumber)
+      .sort('-createdAt');
+
     const response = products.map(product => ({
       ...product._doc,
       _id: product._id.toString(),
     }));
 
-    return NextResponse.json(response, {
-      status: 200,
-    });
+    const count = await ProductModel.countDocuments();
+
+    return NextResponse.json(
+      {
+        products: response,
+        totalCount: count,
+      },
+      {
+        status: 200,
+      },
+    );
   } catch (error) {
     console.error(error);
 
@@ -49,10 +68,13 @@ export async function GET() {
   }
 }
 
-export async function Delete(id: string, publicId: string) {
+export async function Delete(req: NextRequest) {
   try {
+    await connectDB();
+    const { _id, publicId } = await req.json();
+
     await Promise.all([
-      ProductModel.findOneAndDelete({ _id: id }),
+      ProductModel.findOneAndDelete({ _id }),
       deleteImages(publicId),
     ]);
 
