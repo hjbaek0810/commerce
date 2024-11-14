7c9c08c3e6448aab1f43e754bcea460e7fa58b59
hjbaek0810 <doolgi0810@naver.com>
2024-11-14 18:57:14 +0900

    Feat: modal provider 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
-import '@styles/global.css';
+import { ToastContainer } from 'react-toastify';
 
-import '@fortawesome/fontawesome-svg-core/styles.css';
-
-import 'react-toastify/dist/ReactToastify.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
 
 import { sprinkles } from '@styles/sprinkles.css';
 
 import type { Metadata } from 'next';
 
-import { config } from '@fortawesome/fontawesome-svg-core';
+import 'react-toastify/dist/ReactToastify.css';
 
-import { ToastContainer } from 'react-toastify';
+import '@styles/global.css';
+
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+import ModalsProvider from '@utils/modals/provider';
 
 config.autoAddCss = false;
 
@@ -27,26 +29,28 @@ const RootLayout = ({
   return (
     <html lang="en">
       <body>
-        <div
-          className={sprinkles({
-            paddingX: 'spacing-024',
-            paddingY: 'spacing-032',
-            minHeight: 'sizing-full-screen',
-          })}
-        >
-          {children}
-        </div>
-        <ToastContainer
-          position="bottom-right"
-          autoClose={5000} // 자동 off 시간
-          hideProgressBar={false} // 진행시간바 숨김
-          closeOnClick // 클릭으로 알람 닫기
-          rtl={false} // 알림 좌우 반전
-          pauseOnFocusLoss // 화면을 벗어나면 알람 정지
-          draggable // 드래그 가능
-          pauseOnHover // 마우스를 올리면 알람 정지
-          theme="light"
-        />
+        <ModalsProvider>
+          <div
+            className={sprinkles({
+              paddingX: 'spacing-024',
+              paddingY: 'spacing-032',
+              minHeight: 'sizing-full-screen',
+            })}
+          >
+            {children}
+          </div>
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000} // 자동 off 시간
+            hideProgressBar={false} // 진행시간바 숨김
+            closeOnClick // 클릭으로 알람 닫기
+            rtl={false} // 알림 좌우 반전
+            pauseOnFocusLoss // 화면을 벗어나면 알람 정지
+            draggable // 드래그 가능
+            pauseOnHover // 마우스를 올리면 알람 정지
+            theme="light"
+          />
+        </ModalsProvider>
       </body>
     </html>
   );
