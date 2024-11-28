f6c8be9a2605c66e2c37d6624574e5d77c3b8400
hjbaek0810 <doolgi0810@naver.com>
2024-11-29 00:19:44 +0900

    Refator: todo admin product list

--- app/admin/product/page.tsx
+++ app/admin/product/page.tsx
@@ -1,17 +1,12 @@
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import dynamic from 'next/dynamic';
 import Link from 'next/link';
 
 import Button from '@components/Button';
 import Title from '@components/Title';
 import { sprinkles } from '@styles/sprinkles.css';
 import { PATH } from '@utils/path';
-
-const List = dynamic(() => import('./ProductList'), {
-  ssr: false,
-  loading: () => <div>로딩 중...</div>,
-});
+import AdminProductListTable from 'app/admin/product/AdminProductListTable';
 
 const ProductList = () => {
   // TODO: 필터 검색 기능
@@ -19,7 +14,7 @@ const ProductList = () => {
   return (
     <>
       <Title>상품 목록</Title>
-      <List />
+      <AdminProductListTable />
 
       <Link
         href={PATH.ADMIN.PRODUCT.REGISTER}
