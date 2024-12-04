c7fa642f0f3b54fa509aec7232b4e9747bb2b1d8
hjbaek0810 <doolgi0810@naver.com>
2024-12-04 23:08:06 +0900

    Fix: prefetch 시 nextauth  headers 추가

--- app/wish-list/page.tsx
+++ app/wish-list/page.tsx
@@ -1,4 +1,5 @@
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import { headers } from 'next/headers';
 
 import { getWishListQueryOptions } from '@services/queries/wish-list/options';
 import { getQueryClient } from '@utils/query/queryClient';
@@ -8,7 +9,7 @@ import WishGrid from './WishGrid';
 const WishList = async () => {
   const queryClient = getQueryClient();
 
-  await queryClient.prefetchQuery(getWishListQueryOptions());
+  await queryClient.prefetchQuery(getWishListQueryOptions(headers()));
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
