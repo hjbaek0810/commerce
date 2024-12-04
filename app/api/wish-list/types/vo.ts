a4d131413d5282d22f0c5c45f7245f77b2a7d06e
hjbaek0810 <doolgi0810@naver.com>
2024-12-04 21:53:46 +0900

    Fix: type update

--- app/api/wish-list/types/vo.ts
+++ app/api/wish-list/types/vo.ts
@@ -1,6 +1,11 @@
 import type { ProductVO } from '@api/product/types/vo';
 
+export type WishListProductVO = Pick<
+  ProductVO,
+  '_id' | 'name' | 'images' | 'price' | 'salePrice'
+>;
+
 export interface WishListVO {
   _id: string;
-  items: Array<ProductVO>;
+  items: Array<WishListProductVO>;
 }
