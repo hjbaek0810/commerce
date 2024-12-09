d0e4502c1f18f2c4993b94a967f05f11d8093ef3
hjbaek0810 <doolgi0810@naver.com>
2024-12-10 00:24:51 +0900

    Fix: close button 추

--- components/Modal/templates/FindPostCodeModal.tsx
+++ components/Modal/templates/FindPostCodeModal.tsx
@@ -1,19 +1,30 @@
 import type { Address } from 'react-daum-postcode';
 import DaumPostCode from 'react-daum-postcode';
 
+import Button from '@components/Button';
 import Modal from '@components/Modal';
+
+import type { CustomModalPropsType } from '@components/Modal/types';
 
 type FindPostCodeModalPropsType = {
   onSubmit: (address: Address) => void;
-};
+} & Pick<CustomModalPropsType, 'onClose'>;
 
-const FindPostCodeModal = ({ onSubmit }: FindPostCodeModalPropsType) => {
+const FindPostCodeModal = ({
+  onSubmit,
+  onClose,
+}: FindPostCodeModalPropsType) => {
   return (
     <Modal>
       <Modal.Header>Find Post Code</Modal.Header>
       <Modal.Body>
         <DaumPostCode style={{ height: '45rem' }} onComplete={onSubmit} />
       </Modal.Body>
+      <Modal.Footer>
+        <Button size="medium" onClick={onClose}>
+          Close
+        </Button>
+      </Modal.Footer>
     </Modal>
   );
 };
