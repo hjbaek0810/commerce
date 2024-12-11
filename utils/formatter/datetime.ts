5a8a4d61a6a4cd1ab231cbe221861ee32ca203a8
hjbaek0810 <doolgi0810@naver.com>
2024-12-11 20:57:19 +0900

    Feat: datetime formatter 추가

---
+++ utils/formatter/datetime.ts
@@ -0,0 +1,10 @@
+import { isNil } from 'lodash-es';
+
+export function formatDateTime(dateTime: string) {
+  if (isNil(dateTime)) return '';
+
+  const date = new Date(dateTime);
+  const formattedDate = date.toISOString().slice(0, 19).replace('T', ' ');
+
+  return formattedDate;
+}
