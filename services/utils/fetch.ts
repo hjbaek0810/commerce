c236029e071ceabeacc08cecfa9941234fdcff0d
hjbaek0810 <doolgi0810@naver.com>
2024-12-13 17:57:49 +0900

    Fix: headers 할당 구조 수정

--- services/utils/fetch.ts
+++ services/utils/fetch.ts
@@ -1,3 +1,5 @@
+import { revalidatePath } from 'next/cache';
+
 import { ApiError } from './error';
 
 type FetchDataOptionsType<U> = {
@@ -10,16 +12,33 @@ export async function fetchData<T, U = unknown>(
   method: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'GET',
   options: FetchDataOptionsType<U> = {},
 ) {
-  const fetchOptions: RequestInit = {
-    method,
-    headers: {
-      'Content-Type': 'application/json',
-      ...options.headers,
-    },
+  const headers: Record<string, string> = {
+    'Content-Type': 'application/json',
   };
 
-  if (options.data && method !== 'GET')
-    fetchOptions.body = JSON.stringify(options.data);
+  // options.headers가 존재할 경우 이를 headers에 병합
+  if (options.headers) {
+    if (options.headers instanceof Headers) {
+      // Headers 객체일 경우, 객체로 변환하여 병합
+      options.headers.forEach((value, key) => {
+        headers[key] = value;
+      });
+    } else {
+      // 객체 형태일 경우 그대로 병합
+      Object.assign(headers, options.headers);
+    }
+  }
+
+  const fetchOptions: RequestInit = {
+    method,
+    headers,
+  };
+
+  if (options.data && method !== 'GET') {
+    if (typeof options.data === 'object' && options.data !== null) {
+      fetchOptions.body = JSON.stringify(options.data);
+    }
+  }
 
   // FIXME: 배포환경 url
   const response = await fetch(
@@ -30,9 +49,14 @@ export async function fetchData<T, U = unknown>(
   if (!response.ok) {
     const errorData = await response.json();
 
+    if (response.status === 401 || response.status === 403) {
+      revalidatePath('/');
+    }
+
     const error = new ApiError(
       errorData.message || '서버 요청 실패',
       errorData.code || 'UNKNOWN_ERROR',
+      errorData.data,
     );
 
     throw error;
