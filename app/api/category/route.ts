420411f0d7094bbd1c96e486a00df5f0a79a831d
hjbaek0810 <doolgi0810@naver.com>
2025-01-03 19:20:37 +0900

    Delete

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -4,10 +4,6 @@ import connectDB from '@api/config/connectDB';
 import CategoryModel from '@api/models/category';
 import SubCategoryModel from '@api/models/subCategory';
 
-enum CategoryErrorType {
-  CATEGORY_NOT_FOUND = 'CA-001',
-}
-
 export async function GET() {
   try {
     await connectDB();
