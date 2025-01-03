53a1b1014cf73b31acca9a0f88952362bed6d404
hjbaek0810 <doolgi0810@naver.com>
2025-01-03 19:21:04 +0900

    Fix: refetchType props 추가

--- services/utils/helper.ts
+++ services/utils/helper.ts
@@ -1,7 +1,13 @@
 import type { QueryClient } from '@tanstack/react-query';
 
-export const invalidateQueries = (queryClient: QueryClient, queryKeys: any[]) =>
-  queryKeys.forEach(queryKey => queryClient.invalidateQueries({ queryKey }));
+export const invalidateQueries = (
+  queryClient: QueryClient,
+  queryKeys: any[],
+  refetchType: 'all' | 'none' | 'active' = 'active',
+) =>
+  queryKeys.forEach(queryKey =>
+    queryClient.invalidateQueries({ queryKey, refetchType }),
+  );
 
 export const removeQueries = (queryClient: QueryClient, queryKeys: any[]) =>
   queryKeys.forEach(queryKey => queryClient.removeQueries({ queryKey }));
