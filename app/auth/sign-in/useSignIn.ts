a9d7140c7ed24f74c383272c1937605aba0dd5c0
hjbaek0810 <doolgi0810@naver.com>
2025-01-06 19:33:34 +0900

    Refactor: 리팩토링 및 주석 추가

--- app/auth/sign-in/useSignIn.ts
+++ app/auth/sign-in/useSignIn.ts
@@ -6,7 +6,6 @@ import { getSession } from 'next-auth/react';
 
 import { useSignInMutation } from '@services/queries/user';
 import { UserRoleType } from '@utils/constants/user';
-import { PATH } from '@utils/path';
 
 import type { SignInUser } from '@api/auth/sign-in/types/dto';
 
@@ -24,12 +23,11 @@ const useSignIn = () => {
           toast.error('아이디 또는 패스워드가 일치하지 않습니다.');
         } else {
           const session = await getSession();
+          router.push('/');
 
           if (session?.user.role === UserRoleType.ADMIN) {
-            router.push(PATH.ADMIN.HOME);
+            // 사용자가 ADMIN 역할인 경우, 페이지를 새로고침하여 공통 레이아웃(Layout.tsx)에 관리자 전용 레이아웃만 렌더링되도록..
             router.refresh();
-          } else {
-            router.push('/');
           }
         }
       },
