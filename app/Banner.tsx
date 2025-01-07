3f9e8eaebcb455a180d036e353b2c8e30793b991
hjbaek0810 <doolgi0810@naver.com>
2025-01-07 18:37:43 +0900

    Fix: banner 추가

--- app/Banner.tsx
+++ app/Banner.tsx
@@ -1,61 +1,32 @@
 'use client';
 
-import Head from 'next/head';
-
 import Slider from '@components/Slider';
 
-// TODO
 const imgInfo = [
   {
-    src: 'https://picsum.photos/id/1011/1024/600',
-    alt: '',
-    redirectTo: '/',
+    src: '/assets/banner1.png',
+    alt: 'welcome to 2025 한정 혜택 이벤트 진행 중',
   },
   {
-    src: 'https://picsum.photos/id/1012/1024/600',
-    alt: '',
-    redirectTo: '/',
+    src: '/assets/banner2.png',
+    alt: 'ELEVATE YOUR STYLE WITH FUTURISTIC DESIGN BOUNDLESS CREATIVITY',
   },
   {
-    src: 'https://picsum.photos/id/1013/1024/600',
-    alt: '',
-    redirectTo: '/',
+    src: '/assets/banner3.png',
+    alt: 'Minimal Yet Powerful',
   },
 ];
 
 const Banner = () => (
-  <>
-    {/* ssr example */}
-    <Head>
-      <meta
-        property="og:url"
-        content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
-      />
-      <meta property="og:type" content="article" />
-      <meta property="og:title" content="When Great Minds Don’t Think Alike" />
-      <meta
-        property="og:description"
-        content="How much does culture influence creative thinking?"
-      />
-      <meta
-        property="og:image"
-        content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
-      />
-    </Head>
-    <Slider type="fade" height="30vw">
-      <Slider.List>
-        {imgInfo.map((item, index) => (
-          <Slider.Item key={index}>
-            <Slider.ClickableImage
-              src={item.src}
-              alt={item.alt}
-              redirectTo={item.redirectTo}
-            />
-          </Slider.Item>
-        ))}
-      </Slider.List>
-    </Slider>
-  </>
+  <Slider type="fade" height="40vw">
+    <Slider.List>
+      {imgInfo.map((item, index) => (
+        <Slider.Item key={index}>
+          <Slider.ClickableImage src={item.src} alt={item.alt} />
+        </Slider.Item>
+      ))}
+    </Slider.List>
+  </Slider>
 );
 
 export default Banner;
