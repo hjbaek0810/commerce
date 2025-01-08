96dbcf821d9f8979d3ac31a3aad18b2271509508
hjbaek0810 <doolgi0810@naver.com>
2025-01-08 16:32:26 +0900

    Fix: window 는 서버에서 사용 불가능

--- services/utils/fetch.ts
+++ services/utils/fetch.ts
@@ -45,7 +45,7 @@ export async function fetchData<T, U = unknown>(
   }
 
   const response = await fetch(
-    `${window.location.origin}/api/${url}`,
+    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/${url}`,
     fetchOptions,
   );
 
