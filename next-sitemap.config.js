c7040ae8d06e96f6d1aa564df4a309194610a85d
hjbaek0810 <doolgi0810@naver.com>
2025-02-18 17:29:50 +0900

    Chore: sitemap update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_VERCEL_URL, // .게시하는 site의 url
+  siteUrl:
+    process.env.NEXT_PUBLIC_VERCEL_URL ||
+    'https://commerce-mini-mall.vercel.app', // .게시하는 site의 url
   generateRobotsTxt: true, // robots.txt generate 여부 (자동생성 여부)
   sitemapSize: 7000, // sitemap별 최대 크기 (최대 크기가 넘어갈 경우 복수개의 sitemap으로 분리됨)
   changefreq: 'daily', // 페이지 주소 변경 빈도 (검색엔진에 제공됨) - always, daily, hourly, monthly, never, weekly, yearly 중 택 1
   priority: 1, // 페이지 주소 우선순위 (검색엔진에 제공됨, 우선순위가 높은 순서대로 크롤링함)
   exclude: ['/api/*', '/admin/*'], // sitemap 등록 제외 페이지 주소
+  host:
+    process.env.NEXT_PUBLIC_VERCEL_URL ||
+    'https://commerce-mini-mall.vercel.app',
+  sitemap: `${process.env.NEXT_PUBLIC_VERCEL_URL || 'https://commerce-mini-mall.vercel.app'}/sitemap.xml`,
   robotsTxtOptions: {
     // 정책 설정
     policies: [
